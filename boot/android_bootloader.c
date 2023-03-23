115a4e13bda88ae6cbf6f07dd632d613166f39d7
Dmitrii Merkurev <dimorinny@google.com>
2023-03-23 17:32:31 +0000

    ANDROID: Enable u-boot bootloader boot mode back
    Since fastboot is supported, enable bootloader boot mode back
    and provide fastboot tcp default environment to start TCP fastbooot
    
    Test: launch_cvd + adb reboot bootloader + fastboot connect
    Bug: 274924654
    Change-Id: Ie9111e10c101f92544fb0e32a6aef3753841583d
    Signed-off-by: Dmitrii Merkurev <dimorinny@google.com>

--- boot/android_bootloader.c
+++ boot/android_bootloader.c
@@ -563,11 +563,6 @@ int android_bootloader_boot_flow(const char* iface_str,
 	if (misc_part_info) {
 		mode = android_bootloader_load_and_clear_mode(dev_desc, misc_part_info);
 		printf("ANDROID: reboot reason: \"%s\"\n", android_boot_mode_str(mode));
-		// TODO (rammuthiah) fastboot isn't suported on cuttlefish yet.
-		// Once it is, these lines can be removed.
-		if (mode == ANDROID_BOOT_MODE_BOOTLOADER) {
-			mode = ANDROID_BOOT_MODE_NORMAL;
-		}
 	}
 
 	if (!verify) {
