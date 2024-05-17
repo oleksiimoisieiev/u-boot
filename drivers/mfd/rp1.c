29ef93dd738753dd94f735ddf48bea0c091ed69e
Oleksii Moisieiev <oleksii_moisieiev@epam.com>
2024-05-17 12:29:55 +0300

    drivers/mfd/rp1.c
    Signed-off-by: Oleksii Moisieiev <oleksii_moisieiev@epam.com>

--- drivers/mfd/rp1.c
+++ drivers/mfd/rp1.c
@@ -15,7 +15,8 @@
 #include <asm/io.h>
 #include <linux/io.h>
 #include <dm/of_access.h>
-
+#include <dm/simple_bus.h>
+#include <common.h>
 #define RP1_B0_CHIP_ID 0x10001927
 #define RP1_C0_CHIP_ID 0x20001927
 
@@ -63,21 +64,29 @@ void rp1_get_platform(u32 *chip_id, u32 *platform)
 
 static int rp1_get_bar_region(struct udevice *dev, phys_addr_t *bar_start)
 {
-	struct pci_controller *hose;
 
-	if (!dev->parent) {
-		dev_err(dev, "could not find parent device\n");
-		return -EINVAL;
-	}
+//	struct pci_controller *hose;
+//	if (!dev->parent) {
+//		dev_err(dev, "could not find parent device\n");
+//		return -EINVAL;
+//	}
 
-	hose = dev_get_uclass_priv(dev->parent);
 
-	if (!hose || !hose->region_count) {
-		dev_err(dev, "parent is not pci-controller device\n");
-		return -EINVAL;
-	}
 
-	*bar_start = hose->regions[0].phys_start;
+	*bar_start = dm_pci_map_bar(dev, PCI_BASE_ADDRESS_1, 0,0,
+			PCI_REGION_TYPE, PCI_REGION_MEM);
+//	hose = dev_get_uclass_priv(dev->parent->parent);
+//	PP("dev name = %s,",dev->name);
+//	PP("dev par name = %s,",dev->parent->name);
+//	PP("dev par name = %s,",dev->parent->parent->name);
+//	PP("dev = %p hose = %p,",dev->parent, hose);
+//	PP("cnt = %x,", hose->region_count);
+//	if (!hose || !hose->region_count) {
+//		dev_err(dev, "parent is not pci-controller device\n");
+//		return -EINVAL;
+//	}
+//	PP("phys = %llx\n",*bar_start);
+//	*bar_start = hose->regions[0].phys_start;
 	return 0;
 }
 
@@ -85,36 +94,103 @@ static int rp1_probe(struct udevice *dev)
 {
 	int ret;
 	struct rp1_dev *rp1 = dev_get_priv(dev);
+	struct simple_bus_plat *plat = dev_get_uclass_plat(dev);
 	struct device_node *rp1_node;
-
+	struct udevice *bus;
+	PP("");
 	/* Turn on bus-mastering */
 	dm_pci_clrset_config16(dev, PCI_COMMAND, 0, PCI_COMMAND_MASTER | PCI_COMMAND_MEMORY);
+	PP("dev->name = %s", dev->name);
+	//TODO : TODO
+	dm_pci_write_config32(dev, PCI_BASE_ADDRESS_1,0);
 
 	ret = rp1_get_bar_region(dev, &rp1->bar_start);
 	if (ret)
 		return ret;
+	PP("rp1-bar_start = %llx", rp1->bar_start);
 
 	/* Get chip id */
 	g_chip_id = rp1_reg_read(rp1, RP1_SYSINFO_BASE, SYSINFO_CHIP_ID_OFFSET);
 	g_platform = rp1_reg_read(rp1, RP1_SYSINFO_BASE, SYSINFO_PLATFORM_OFFSET);
-	dev_dbg(dev, "chip_id 0x%x%s\n", g_chip_id,
+	PP("chip_id 0x%x%s\n", g_chip_id,
 		(g_platform & RP1_PLATFORM_FPGA) ? " FPGA" : "");
+	PP("");
 
 	if (g_chip_id != RP1_C0_CHIP_ID) {
 		dev_err(dev, "wrong chip id (%x)\n", g_chip_id);
 		return -EINVAL;
 	}
 
-	for_each_of_allnodes(rp1_node) {
-		if (rp1_node->name && strcmp("rp1", rp1_node->name))
-			break;
-	}
-
 	g_rp1 = rp1;
 
+	plat->base = rp1->bar_start;
+	plat->target = 0;
+	plat->size = 0x400000;
 	return 0;
 }
 
+static int rp1_bind(struct udevice *dev)
+{
+	struct device_node *rp1_node;
+//	struct udevice *ndev;
+//	int ret;
+//	PP("dev- = %p", dev);
+//	PP("dev->driver = %p", dev->driver);
+//	PP("dev->name = %s", dev->name);
+//	PP("dev->name = %s", dev->driver->name);
+//	for_each_of_allnodes(rp1_node) {
+//		if (rp1_node->name && (strcmp("rp1", rp1_node->name) == 0)) {
+//			PP("found->name = %s", rp1_node->name);
+//
+//			break;
+//		}
+//	}
+//	ret = device_find_global_by_ofnode(np_to_ofnode(rp1_node), &ndev);
+//	PP("ret = %d", ret);
+//	PP("dev- = %p", ndev);
+//	PP("dev->driver = %p", ndev->driver);
+//	PP("dev->name = %s", ndev->name);
+//	PP("dev->name = %s", ndev->driver->name);
+//	ndev->driver = dev->driver;
+	PP("");
+	device_set_name(dev, "rp1");
+	return 0;
+}
+static int rp1_bus_post_bind(struct udevice *dev)
+{
+//	struct rp1_dev *plat = dev_get_uclass_plat(dev);
+//	int ret;
+//
+//	if (CONFIG_IS_ENABLED(SIMPLE_BUS_CORRECT_RANGE)) {
+//		uint64_t caddr, paddr, len;
+//
+//		/* only read range index 0 */
+//		ret = fdt_read_range((void *)gd->fdt_blob, dev_of_offset(dev),
+//				     0, &caddr, &paddr, &len);
+//		if (!ret) {
+//			plat->base = caddr;
+//			plat->target = paddr;
+//			plat->size = len;
+//		}
+//	} else {
+//		u32 cell[3];
+//
+//		ret = dev_read_u32_array(dev, "ranges", cell,
+//					 ARRAY_SIZE(cell));
+//		if (!ret) {
+//			plat->bar_start = cell[0];
+//			plat->target = cell[1];
+//			plat->size = cell[2];
+//		}
+//	}
+	PP("");
+	return dm_scan_fdt_dev(dev);
+}
+static int rp1_of_to_plat(struct udevice *dev)
+{
+	PP("");
+	return 0;
+}
 static const struct pci_device_id dev_id_table[] = {
 	{ PCI_DEVICE(PCI_VENDOR_ID_RPI, PCI_DEVICE_ID_RP1_C0), },
 	{ 0, }
@@ -143,17 +219,26 @@ static const struct dm_pci_ops rp1_pcie_ops = {
  */
 
 static const struct udevice_id rp1_ids[] = {
-	{.compatible = "brcm,rp1-simple-bus"},
-	{}
+		{.compatible = "sss-bus"},
+		{}
 };
 
+UCLASS_DRIVER(rp1_driver) = {
+	.id		= UCLASS_SIMPLE_BUS,
+	.name		= "rp1_driver",
+	.post_bind = rp1_bus_post_bind,
+	.per_device_plat_auto	= sizeof(struct simple_bus_plat),
+};
 U_BOOT_DRIVER(rp1_driver) = {
 	.name			= "rp1_driver",
 	.id			= UCLASS_SIMPLE_BUS,
 	.probe			= rp1_probe,
+	.bind 			= rp1_bind,
+	.of_to_plat		= rp1_of_to_plat,
 	.priv_auto		= sizeof(struct rp1_dev),
 	.ops			= &rp1_pcie_ops,
-	.of_match		= rp1_ids,
+	//.of_match		= rp1_ids,
+
 };
 
 U_BOOT_PCI_DEVICE(rp1_driver, dev_id_table);
